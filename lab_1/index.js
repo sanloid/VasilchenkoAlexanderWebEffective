1d74e03dac178875f521add0300fc13942df8dc2
Alexand Vasilchenko <sanloid2@gamil.com>
2022-11-04 23:41:08 +0600

    lab1

---
+++ lab_1/index.js
@@ -0,0 +1,10 @@
+
+function showmenu() {
+    const innerDiv = document.querySelector('.list')
+    innerDiv.style.display = "grid"
+}
+
+function hidemenu() {
+    const innerDiv = document.querySelector('.list')
+    innerDiv.style.display = "none"
+}
