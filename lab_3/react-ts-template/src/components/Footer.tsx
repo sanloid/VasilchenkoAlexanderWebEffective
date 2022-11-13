2a84a98da5da0fc83fbed4e8f84d556de3bece0f
sanloid <sanloid2@gamil.com>
2022-11-13 20:49:52 +0600

    feat: calcultaing current year

--- lab_3/react-ts-template/src/components/Footer.tsx
+++ lab_3/react-ts-template/src/components/Footer.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 export default function Footer() {
+
     return (
         <footer className="text-white body-font bg-red-acid">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col justify-between">
                 <a className="flex items-center md:justify-start justify-center h-10">
                     <img src="./marvel_logo.svg" className="w-full h-full" alt="" />
                 </a>
-                <p className="text-sm sm:ml-4 sm:pl-4 sm:border-white sm:py-2 sm:mt-0 mt-4">Data provided by Marvel. © 2022 MARVEL
+                <p className="text-sm sm:ml-4 sm:pl-4 sm:border-white sm:py-2 sm:mt-0 mt-4">Data provided by Marvel. © {new Date().getFullYear()} MARVEL
                 </p>
                 <a href="https://developer.marvel.com" className="ml-1" rel="noopener noreferrer" target="_blank">developer.marvel.com</a>
             </div>
