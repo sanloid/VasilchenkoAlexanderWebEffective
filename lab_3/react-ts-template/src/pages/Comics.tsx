15d10014232274af8f10a64ad9a13e752013061d
sanloid <sanloid2@gamil.com>
2022-11-13 19:25:53 +0600

    feat: add comics page

--- lab_3/react-ts-template/src/pages/Comics.tsx
+++ lab_3/react-ts-template/src/pages/Comics.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Card from '../components/Card'
 
 export default function Comics() {
+    const comicsArr = [
+        { name: "New Mutants: Facsimile Edition (2022) #98", desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!", img: "https://i.annihil.us/u/prod/marvel/i/mg/9/50/632cd36b4da77/portrait_uncanny.jpg" },
+        { name: "New Mutants: Facsimile Edition (2022) #98", desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!", img: "https://i.annihil.us/u/prod/marvel/i/mg/d/60/63658cf8efef2/portrait_uncanny.jpg" },
+        { name: "New Mutants: Facsimile Edition (2022) #98", desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!", img: "https://i.annihil.us/u/prod/marvel/i/mg/9/50/632cd36b4da77/portrait_uncanny.jpg" },
+        { name: "New Mutants: Facsimile Edition (2022) #98", desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!", img: "https://i.annihil.us/u/prod/marvel/i/mg/d/60/63658cf8efef2/portrait_uncanny.jpg" },
+        { name: "New Mutants: Facsimile Edition (2022) #98", desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!", img: "https://i.annihil.us/u/prod/marvel/i/mg/9/50/632cd36b4da77/portrait_uncanny.jpg" },
+        { name: "New Mutants: Facsimile Edition (2022) #98", desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!", img: "https://i.annihil.us/u/prod/marvel/i/mg/d/60/63658cf8efef2/portrait_uncanny.jpg" },
+    ]
     return (
-        <div>Comics</div>
+        <>
+            <section className="text-gray-600 body-font min-h-screen">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+                        {comicsArr.map(e =>
+                            <Card name={e.name} desc={e.desc} img={e.img} />
+                        )}
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
