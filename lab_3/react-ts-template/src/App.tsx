d87d549310651d1b6b02206cf490528ec9c06c98
sanloid <sanloid2@gamil.com>
2022-11-13 19:21:23 +0600

    feat: layout and routing

--- lab_3/react-ts-template/src/App.tsx
+++ lab_3/react-ts-template/src/App.tsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Characters from './pages/Characters'
+import './index.css'
+import Series from './pages/Series'
+import Comics from './pages/Comics'
+import { Routes, Route } from "react-router-dom";
+import Layout from './Layout'
 
 function App() {
 
   return (
-    <div className="App">
-
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Characters />} />
+        <Route path="comics" element={<Comics />} />
+        <Route path="series" element={<Series />} />
+        {/* <Route path="*" element={<NoMatch />} /> */}
+      </Route>
+    </Routes>
   )
 }
 
--- lab_3/react-ts-template/src/components/Header.tsx
+++ lab_3/react-ts-template/src/components/Header.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
-export default function Header() {
+const Header: React.FC = () => {
+    const linksArr = [
+        { name: "Characters", path: "/" },
+        { name: "Comics", path: "comics" },
+        { name: "Series", path: "series" }
+    ];
+    let active = "border-b-2";
+    let linkStyles = "cursor-pointer text-white text-xl hover:rounded-xl hover:bg-red-100 p-5 mr-5 hover:text-gray-900"
     return (
-        <div>Header</div>
+        <header className="text-gray-600 font-marvel bg-red-acid">
+            <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
+                <a className="flex items-center h-10">
+                    <img src="./marvel_logo.svg" className="w-full h-full" alt="" />
+                </a>
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                    {linksArr.map((e, i) =>
+                        <NavLink to={e.path}  className={linkStyles}>{e.name}</NavLink>
+                    )}
+                </nav>
+            </div>
+        </header>
     )
 }
+
+export default Header;
