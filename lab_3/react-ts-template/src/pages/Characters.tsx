f84c54515bf43326c3ac4f4dcc43d87e083cdab3
sanloid <sanloid2@gamil.com>
2022-11-13 19:25:25 +0600

    feat: add characters page

--- lab_3/react-ts-template/src/pages/Characters.tsx
+++ lab_3/react-ts-template/src/pages/Characters.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Card from '../components/Card'
 
 export default function Characters() {
+    const CharactersArr = [
+        { name: "DOCTOR STRANGE", desc: "Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.", img: "https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg" },
+        { name: "DOCTOR STRANGE", desc: "Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.", img: "https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg" },
+        { name: "DOCTOR STRANGE", desc: "Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.", img: "https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg" },
+        { name: "DOCTOR STRANGE", desc: "Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.", img: "https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg" },
+        { name: "DOCTOR STRANGE", desc: "Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.", img: "https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg" },
+    ]
     return (
-        <div>Characters</div>
+        <>
+            <section className="text-gray-600 body-font min-h-screen">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+                        {CharactersArr.map(e =>
+                            <Card name={e.name} desc={e.desc} img={e.img} />
+                        )}
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
