0391491a4994f0fd68d94cfca771d3a3dd40a5d7
sanloid <sanloid2@gamil.com>
2022-11-13 20:04:13 +0600

    fix: useless imports

--- lab_3/react-ts-template/src/App.tsx
+++ lab_3/react-ts-template/src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import Footer from './components/Footer'
-import Header from './components/Header'
 import Characters from './pages/Characters'
 import './index.css'
 import Series from './pages/Series'
