11f658eb588dd7ce63d3d367c36173e6e69873eb
sanloid <sanloid2@gamil.com>
2022-11-13 19:40:45 +0600

    feat: remove arrow from ref

--- lab_3/react-ts-template/src/components/Card.tsx
+++ lab_3/react-ts-template/src/components/Card.tsx
@@ -17,9 +17,6 @@ const Card: React.FC<CardPropType> = ({ name, desc, img }) => {
                 <p className="text-base leading-relaxed mt-2">{desc}</p>
                 <a className="cursor-pointer text-red-600 border-2 p-2 rounded-xl border-red-600 hover:bg-red-600 hover:text-white inline-flex items-center mt-3">
                     See More
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
                 </a>
             </div>
         </>
