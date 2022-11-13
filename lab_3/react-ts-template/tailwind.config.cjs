fd6ef1f1b03f0d03476c9e651d22f9f4be78da82
sanloid <sanloid2@gamil.com>
2022-11-13 19:20:37 +0600

    feat: add tailwindcss

---
+++ lab_3/react-ts-template/tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'red-acid': 'rgb(207, 19, 19)',
+      },
+      fontFamily: {
+        'marvel': ['Marvel'],
+      },
+    },
+  },
+  plugins: [],
+}
