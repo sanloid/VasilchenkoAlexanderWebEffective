ca846ddcc102ec8ea07d49a48444a5fd9d1a1caa
sanloid <sanloid2@gamil.com>
2022-11-18 16:34:25 +0600

    fix: unique id

--- lab_3/react-ts-template/src/pages/Series/Series.tsx
+++ lab_3/react-ts-template/src/pages/Series/Series.tsx
@@ -32,7 +32,7 @@ export const seriesArr = [
     name: 'New Mutants: Facsimile Edition (2022) #98',
     desc: 'In the The Guardians of the Galaxy Holiday Special, the Guardians, who are on a mission to make Christmas unforgettable for Quill, head to Earth in search of the perfect present. The Marvel Studios’ Special Presentation stars Chris Pratt, Dave Bautista, Karen Gillan, and Pom Klementieff, featuring Vin Diesel as Groot and Bradley Cooper as Rocket, Sean Gunn and The Old 97’s with Michael Rooker and Kevin Bacon.',
     img: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/theguardiansofthegalaxyholidayspecial_lob_crd_02.jpg',
-    id: '5'
+    id: v4()
   },
   {
     name: 'New Mutants: Facsimile Edition (2022) #98',
