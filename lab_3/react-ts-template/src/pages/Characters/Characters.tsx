3829cf5ecdafa261f237538ac38bd3a965cc398e
sanloid <sanloid2@gamil.com>
2022-11-18 15:42:26 +0600

    feat: global arrays

--- lab_3/react-ts-template/src/pages/Characters/Characters.tsx
+++ lab_3/react-ts-template/src/pages/Characters/Characters.tsx
@@ -3,39 +3,40 @@ import { v4 } from 'uuid';
 import Card from '../../components/Card';
 import SearchForm from '../../components/SearchForm';
 
+export const CharactersArr = [
+  {
+    name: 'DOCTOR STRANGE',
+    desc: 'Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.',
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg',
+    id: v4()
+  },
+  {
+    name: 'DOCTOR STRANGE',
+    desc: 'Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.',
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg',
+    id: v4()
+  },
+  {
+    name: 'DOCTOR STRANGE',
+    desc: 'Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.',
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg',
+    id: v4()
+  },
+  {
+    name: 'DOCTOR STRANGE',
+    desc: 'Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.',
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg',
+    id: v4()
+  },
+  {
+    name: 'DOCTOR STRANGE',
+    desc: 'Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.',
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg',
+    id: v4()
+  }
+];
+
 const Characters: React.FC = () => {
-  const CharactersArr = [
-    {
-      name: 'DOCTOR STRANGE',
-      desc: 'Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.',
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg',
-      id: v4()
-    },
-    {
-      name: 'DOCTOR STRANGE',
-      desc: 'Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.',
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg',
-      id: v4()
-    },
-    {
-      name: 'DOCTOR STRANGE',
-      desc: 'Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.',
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg',
-      id: v4()
-    },
-    {
-      name: 'DOCTOR STRANGE',
-      desc: 'Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.',
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg',
-      id: v4()
-    },
-    {
-      name: 'DOCTOR STRANGE',
-      desc: 'Possessing vast magical knowledge and skills to call upon, he now serves as Earth’s newest Master of the Mystic Arts.',
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/f0/57fe4ef408a29/standard_incredible.jpg',
-      id: v4()
-    }
-  ];
   return (
     <>
       <SearchForm />
@@ -43,7 +44,13 @@ const Characters: React.FC = () => {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
             {CharactersArr.map((e) => (
-              <Card name={e.name} desc={e.desc} img={e.img} id={e.id} />
+              <Card
+                name={e.name}
+                desc={e.desc}
+                img={e.img}
+                id={e.id}
+                key={v4()}
+              />
             ))}
           </div>
         </div>
--- lab_3/react-ts-template/src/pages/Comics/Comics.tsx
+++ lab_3/react-ts-template/src/pages/Comics/Comics.tsx
@@ -3,45 +3,46 @@ import { v4 } from 'uuid';
 import Card from '../../components/Card';
 import SearchForm from '../../components/SearchForm';
 
+export const comicsArr = [
+  {
+    name: 'New Mutants: Facsimile Edition (2022) #98',
+    desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/50/632cd36b4da77/portrait_uncanny.jpg',
+    id: v4()
+  },
+  {
+    name: 'New Mutants: Facsimile Edition (2022) #98',
+    desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/d/60/63658cf8efef2/portrait_uncanny.jpg',
+    id: v4()
+  },
+  {
+    name: 'New Mutants: Facsimile Edition (2022) #98',
+    desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/50/632cd36b4da77/portrait_uncanny.jpg',
+    id: v4()
+  },
+  {
+    name: 'New Mutants: Facsimile Edition (2022) #98',
+    desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/d/60/63658cf8efef2/portrait_uncanny.jpg',
+    id: v4()
+  },
+  {
+    name: 'New Mutants: Facsimile Edition (2022) #98',
+    desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/50/632cd36b4da77/portrait_uncanny.jpg',
+    id: v4()
+  },
+  {
+    name: 'New Mutants: Facsimile Edition (2022) #98',
+    desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
+    img: 'https://i.annihil.us/u/prod/marvel/i/mg/d/60/63658cf8efef2/portrait_uncanny.jpg',
+    id: v4()
+  }
+];
+
 const Comics: React.FC = () => {
-  const comicsArr = [
-    {
-      name: 'New Mutants: Facsimile Edition (2022) #98',
-      desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/50/632cd36b4da77/portrait_uncanny.jpg',
-      id: v4()
-    },
-    {
-      name: 'New Mutants: Facsimile Edition (2022) #98',
-      desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/d/60/63658cf8efef2/portrait_uncanny.jpg',
-      id: v4()
-    },
-    {
-      name: 'New Mutants: Facsimile Edition (2022) #98',
-      desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/50/632cd36b4da77/portrait_uncanny.jpg',
-      id: v4()
-    },
-    {
-      name: 'New Mutants: Facsimile Edition (2022) #98',
-      desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/d/60/63658cf8efef2/portrait_uncanny.jpg',
-      id: v4()
-    },
-    {
-      name: 'New Mutants: Facsimile Edition (2022) #98',
-      desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/50/632cd36b4da77/portrait_uncanny.jpg',
-      id: v4()
-    },
-    {
-      name: 'New Mutants: Facsimile Edition (2022) #98',
-      desc: "The dynamic debut of Deadpool! It's the beginning of the end for the New Mutants as Cable continues his reinvention of the squad into his own paramilitary force!",
-      img: 'https://i.annihil.us/u/prod/marvel/i/mg/d/60/63658cf8efef2/portrait_uncanny.jpg',
-      id: v4()
-    }
-  ];
   return (
     <>
       <SearchForm />
