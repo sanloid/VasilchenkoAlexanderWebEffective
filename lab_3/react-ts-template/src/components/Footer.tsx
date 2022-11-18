1b348c3a146edd1666cc4d23a98d09e4acc45c81
sanloid <sanloid2@gamil.com>
2022-11-18 15:36:57 +0600

    fix: wrong path

--- lab_3/react-ts-template/src/components/Footer.tsx
+++ lab_3/react-ts-template/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     <footer className="text-white body-font bg-red-acid dark:bg-gray-600">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col justify-between">
         <div className="flex items-center md:justify-start justify-center h-10">
-          <img src="./marvel_logo.svg" className="w-full h-full" alt="" />
+          <img src="/marvel_logo.svg" className="w-full h-full" alt="" />
         </div>
         <p className="text-sm sm:ml-4 sm:pl-4 sm:border-white sm:py-2 sm:mt-0 mt-4">
           Data provided by Marvel. ©{new Date().getFullYear()} MARVEL
