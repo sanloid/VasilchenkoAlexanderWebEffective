bfed54ac6272c916b99d9257b10497e47f40b76f
sanloid <sanloid2@gamil.com>
2022-11-28 20:16:58 +0600

    feat: store

---
+++ lab_3/react-ts-template/src/stores/index.ts
@@ -0,0 +1,9 @@
+import CharStore from './CharStore';
+import ComicsStore from './ComicsStore';
+import SeriesStore from './SeriesStore';
+
+export default {
+  CharStore: new CharStore(),
+  ComicsStore: new ComicsStore(),
+  SeriesStore: new SeriesStore()
+};
