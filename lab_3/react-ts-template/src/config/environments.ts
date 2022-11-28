f9f403ebb104023440e264fa2b5aae10c8fb7e73
sanloid <sanloid2@gamil.com>
2022-11-28 20:15:07 +0600

    feat: new env var

--- lab_3/react-ts-template/src/config/environments.ts
+++ lab_3/react-ts-template/src/config/environments.ts
@@ -2,5 +2,6 @@ const envs = import.meta.env;
 
 export default {
   apiKey: envs.VITE_API_KEY,
-  baseApiUrl: envs.VITE_BASE_API_URL
+  baseApiUrl: envs.VITE_BASE_API_URL,
+  privateKey: envs.VITE_API_KEY_PRIVATE
 };
