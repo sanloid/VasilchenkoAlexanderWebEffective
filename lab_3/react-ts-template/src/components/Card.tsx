29ee4085ab170db799a00e758298da83bea9be60
sanloid <sanloid2@gamil.com>
2022-11-29 18:13:20 +0600

    feat: adaptive layout, prop type

--- lab_3/react-ts-template/src/components/Card.tsx
+++ lab_3/react-ts-template/src/components/Card.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { CardPropType } from 'types/Card';
+import { NavLink, useLocation } from 'react-router-dom';
 
-const Card: React.FC<CardPropType> = ({ name, desc, img, id }) => {
+export interface CardPropType {
+  id: string;
+  name: string;
+  description: string;
+  img: {
+    path: string;
+    extension: string;
+  };
+}
+
+const Card: React.FC<CardPropType> = ({ img, description, name, id }) => {
+  const loc = useLocation().pathname.split('/')[1];
   return (
     <>
-      <div className="p-4 md:w-1/3 font-marvel sm:mb-0 mb-6">
+      <div className="p-4 md:w-1/3 sm:w-1/2 xsm:w-1/2 xxsm:w-full xxxsm:w-full font-marvel sm:mb-0 mb-6">
         <div className="rounded-lg h-64 overflow-hidden">
           <img
             alt="content"
             className="object-cover object-top w-full h-full"
-            src={img}
+            src={`${img.path}.${img.extension}`}
           />
         </div>
         <h2 className="text-xl font-medium mt-5">{name}</h2>
-        <p className="text-base leading-relaxed mt-2">{desc}</p>
-        <Link
-          to={id}
+        <p className="text-base leading-relaxed mt-2">{description}</p>
+        <NavLink
+          to={`../${loc}/${id}`}
           className="cursor-pointer text-red-600 dark:text-gray-600 border-2 p-2 rounded-xl border-red-600 dark:border-gray-600 hover:bg-red-600 dark:hover:bg-gray-600 hover:text-white dark:hover:text-white inline-flex items-center mt-3"
         >
           See More
-        </Link>
+        </NavLink>
       </div>
     </>
   );
