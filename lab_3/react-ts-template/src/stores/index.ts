863238d5536415a9e40633ff9a5b753a6026fe17
sanloid <sanloid2@gamil.com>
2022-12-02 19:34:11 +0600

    fix: worng content path

--- lab_3/react-ts-template/src/stores/index.ts
+++ lab_3/react-ts-template/src/stores/index.ts
@@ -2,6 +2,6 @@ import Store from './Store';
 
 export default {
   CharStore: new Store('characters', ['comics', 'series']),
-  ComicsStore: new Store('comics', ['characters', 'comics']),
-  SeriesStore: new Store('series', ['characters'])
+  ComicsStore: new Store('comics', ['characters']),
+  SeriesStore: new Store('series', ['characters', 'comics'])
 };
